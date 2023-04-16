c019ffa0a11ba5ca17b2484285531335eb494909
Hiếu Vũ <85061650+kaiojudo@users.noreply.github.com>
2023-04-16 08:21:56 +0700

    success

--- FrontEnd/src/component/container/leftcontainer/news.jsx
+++ FrontEnd/src/component/container/leftcontainer/news.jsx
@@ -10,7 +10,7 @@ export default function News() {
         setDataPost(data);
       });
   }, []);
-  console.log(datapost);
+  console.log(datapost?.result?.tieudetin);
   return (
     <>
       <div className="linkfrom">
@@ -18,95 +18,28 @@ export default function News() {
         <i className="fa-solid fa-caret-right"></i>
         <span>Game</span>
       </div>
-      
-        <div className="post-details">
-          <label htmlFor="" className="tieude">
-            tiêu đề tin
-          </label>
-          <p className="tacgia">Hiếu vũ</p>
-          <p className="gioithieu">
-            Hàng nghìn Giftcode đang chờ game thủ Cửu Thiên Mobile trải nghiệm.
-          </p>
-          <p>
-            <a href="" className="">
-              Cửu thiên Mobile
-            </a>
-            được đánh giá là ngọn gió mát lành "đúng người đúng thời điểm" trên
-            thị trường game hiện nay. Dù sở hữu toàn bộ những yếu tố tiêu chuẩn
-            về đồ họa và gameplay chuẩn chỉ của dòng game nhập vai nhưng Cửu
-            Thiên Mobile hướng đến giá trị giải trí, vui vẻ, khuây khỏa đúng
-            nghĩa dành cho game thủ Việt - những người vốn đã rất mệt mỏi với
-            cuộc sống cơm, áo, gạo, tiền.
-          </p>
-          <br />
-          <br />
-          <p>
-            <strong>
-              Có thể tải game tại đây:{" "}
-              <a href="">https://cuuthien.onelink.me/855c/taigame1</a>
-            </strong>
-          </p>
-          <img src="../cuuthien.jpg" alt="" />
-          <p>
-            Cửu Thiên Mobile chính thức ra mắt vào 1/7/2022. Nhân dịp này nhà
-            phát hành xin gửi tặng game thủ những mã quà tặng giá trị để tăng
-            phần trải nghiệm khi tham gia trò chơi.​
-          </p>
-          <p className="giftcode-game">CUUTHIENMOBILE</p>
-          <p className="giftcode-game">GAMETIENHIEP</p>
-          <p className="giftcode-game">FANLONGHON</p>
-          <p className="giftcode-game">GAMEHOT2022</p>
-          <p className="giftcode-game">GAME200MB</p>
-          <p style={{ textAlign: "justify" }}>
-            <br />
-            <strong>Điểm qua vài tính năng đáng chơi Cửu Thiên Mobile:</strong>
-            <br />
-            <br />
-            <i>2 bộ liên hoàn chiêu 9 nút, 108 cách chiêu thức:</i> Khác với
-            phong cách bật auto lao vào, tung chiêu bất chấp như các tựa game
-            hiện có, để giành chiến thắng tại Cửu Thiên Mobile, người chơi cần
-            phải căn ke vị trí, thời gian tung chiêu, linh hoạt di chuyển và sắp
-            xếp hợp lý.'
-            <br />
-          </p>
-          <img src="../cuuthien1.jpg" alt="" />
-          <p style={{ textAlign: "justify" }}>
-            <br />
-            <i>Ngũ Hệ tương khắc với 3 ngũ hành: </i>Kim, Mộc, Thổ Nữ có 3 ngũ
-            hành: Kim, Thủy, Hỏa -&gt; 5 lớp nhân vật theo Ngũ hành tương sinh,
-            tương khắc với nhau.
-            <br />
-            <br />
-            <i>Kỹ năng đa dạng, đa dạng tùy chỉnh:</i> Bạn có tới 12 kỹ năng chủ
-            động, có thể ra trận tới 9 kỹ năng. Mỗi kỹ năng có 3 hệ theo từng
-            lớp nhân vật, đặc biệt người chơi có thể tự do xây dựng các kỹ năng
-            theo sở thích để có thể khắc chế những người chơi khác.
-            <br />​
-          </p>
-          ​<img src="../cuuthien2.gif" alt="" />
-          <p style={{ textAlign: "justify" }}>
-            <br />
-            <i>Chiến Trường Tiên Minh</i>: Chiến trường nơi đây cho phép 16 Tiên
-            Minh mạnh nhất đấu xuyên suốt để tìm ra người thắng cuộc, cũng chính
-            là sự gắn kết, thể hiện tính đoàn kết giữa các bang hội với nhau.
-            <br />
-            <br />
-            <i>Chợ Trời - Nơi mua bán các vật phẩm quan trọng:</i> Đặc biệt, chỉ
-            có thể kiếm các trang bị hiếm (trang bị đỏ) từ tính năng này.
-            <br />​
-          </p>
-          <img src="../cuuthien3.jpg" alt="" />
-          <p style={{ textAlign: "justify" }}>
-            <br />
-            Cho đến thời điểm hiện tại, không có nhiều những tựa game "dễ chịu"
-            như Cửu Thiên Mobile. Mặc dù vậy nhưng tựa game này vẫn không "quên"
-            mang đến những gì thú vị nhất, cuốn hút nhất trong gameplay cho game
-            thủ. Không những vậy, những tính năng mà Cửu Thiên Mobile sở hữu đều
-            thuộc dạng "độc - lạ" trên thị trường hiện nay, chắc chắn sẽ mang
-            đến cho game thủ những trải nghiệm đặc sắc và khó quên.​
-          </p>
-        </div>
-     
+
+      <div className="post-details">
+        <label htmlFor="" id="preshow-name">
+          {datapost?.result?.tieudetin}
+        </label>
+        <img
+          src={datapost?.result?.hinhtrichdan}
+          alt=""
+          id="prehinhtrichdan"
+        ></img>
+        <p id="preshow-1">{datapost?.result?.doan1}</p>
+        <img src={datapost?.result?.anh1} alt="" id="img1" />
+        <p id="preshow-2">{datapost?.result?.doan2}</p>
+        <img src={datapost?.result?.anh2} alt="" id="img2" />
+        <p id="preshow-3">{datapost?.result?.doan3}</p>
+        <img src={datapost?.result?.anh3} alt="" id="img3" />
+        <p id="preshow-4">{datapost?.result?.doan4}</p>
+        <p id="tacgia">
+          Tác giả:
+          <i></i>
+        </p>
+      </div>
     </>
   );
 }
