4e868664535f977204ead83444b7fa72f1102ec4
Hiếu Vũ <85061650+kaiojudo@users.noreply.github.com>
2023-05-10 15:08:18 +0700

    .

--- BackEnd/app/model/theloai.model.js
+++ BackEnd/app/model/theloai.model.js
@@ -1,53 +1,68 @@
-var db = require('../common/connect');
+var db = require("../common/connect");
 
 const Theloai = function (theloai) {
-    this.idTheloai = theloai.idTheloai;
-    this.tenTheloai = theloai.tenTheloai;
-    this.isNull = theloai.isNull;
-    this.isNull_Danhmuc = theloai.isNull_Danhmuc;
-    this.sapxep = theloai.sapxep;
-    this.linkngoai = theloai.linkngoai;
-    this.target = theloai.target;
-    this.url = theloai.url;
-}
+  this.idTheloai = theloai.idTheloai;
+  this.tenTheloai = theloai.tenTheloai;
+  this.isNull = theloai.isNull;
+  this.sapxep = theloai.sapxep;
+};
 Theloai.get_all = function (result) {
   db.query(`SELECT * from theloai WHERE isNull = 1`, function (err, theloai) {
-      if (err) {
-          result(err);
-      } else {
-          result(theloai);
-      }
+    if (err) {
+      result(err);
+    } else {
+      result(theloai);
+    }
   });
-}
+};
 Theloai.get_header = function (result) {
-    db.query(`SELECT * from theloai WHERE sapxep <= 6 AND isNull = 1`, function (err, theloai) {
-        if (err) {
-            result(err);
-        } else {
-            result(theloai);
-        }
-    });
-}
-Theloai.details = function (idTheloai, result) {
-    db.query(`SELECT * from theloai WHERE idTheloai = ? `, idTheloai, function (err, theloai) {
-        if (err) {
-            result(err);
-        } else {
-            result(theloai[0]);
-        }
-    });
-}
-Theloai.delete = function (idtheloai, result) {
-    db.query(
-      `UPDATE gcms.theloai SET isNull = 0 WHERE idtheloai = ? ;`,
-      idtheloai,
-      function (err, data) {
-        if (err) {
-          result(null);
-        } else {
-          result(data);
-        }
+  db.query(
+    `SELECT * from theloai WHERE sapxep <= 6 AND isNull = 1`,
+    function (err, theloai) {
+      if (err) {
+        result(err);
+      } else {
+        result(theloai);
       }
-    );
-  };
+    }
+  );
+};
+Theloai.details = function (idTheloai, result) {
+  db.query(
+    `SELECT * from theloai WHERE idTheloai = ? `,
+    idTheloai,
+    function (err, theloai) {
+      if (err) {
+        result(err);
+      } else {
+        result(theloai[0]);
+      }
+    }
+  );
+};
+Theloai.delete = function (idtheloai, result) {
+  db.query(
+    `UPDATE gcms.theloai SET isNull = 0 WHERE idtheloai = ? ;`,
+    idtheloai,
+    function (err, data) {
+      if (err) {
+        result(null);
+      } else {
+        result(data);
+      }
+    }
+  );
+};
+Theloai.add_new = function (data, result) {
+  db.query(`INSERT INTO theloai SET ? `, data, function (err, res) {
+    if (err) {
+      result(err);
+    } else {
+      result({
+        idTheLoai: res.insertId,
+        ...data,
+      });
+    }
+  });
+};
 module.exports = Theloai;
