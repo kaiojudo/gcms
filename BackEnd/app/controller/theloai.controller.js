a83d50b3006b9a7887a8852c7e102ed13b1119dd
Hiếu Vũ <85061650+kaiojudo@users.noreply.github.com>
2023-05-10 15:13:44 +0700

    .

--- BackEnd/app/controller/theloai.controller.js
+++ BackEnd/app/controller/theloai.controller.js
@@ -1,30 +1,38 @@
-var Theloai = require('../model/theloai.model')
+var Theloai = require("../model/theloai.model");
 
 exports.get_header = function (req, res) {
-    Theloai.get_header(function (data) {
-        res.send({
-            result: data
-        });
-    })
-}
-exports.get_all = function (req, res) {
-    Theloai.get_all(function (data) {
-        res.send({
-            result: data
-        });
-    })
-}
-exports.details = function (req, res) {
-    Theloai.details(req.params.id, function (child) {
-        res.send({
-            result: child
-        });
-    })
-}
-exports.delete = function (req, res) {
-    Theloai.delete(req.params.id, function (deleteItem) {
-      res.send({
-        result: deleteItem,
-      });
+  Theloai.get_header(function (data) {
+    res.send({
+      result: data,
     });
-  };
+  });
+};
+exports.get_all = function (req, res) {
+  Theloai.get_all(function (data) {
+    res.send({
+      result: data,
+    });
+  });
+};
+exports.details = function (req, res) {
+  Theloai.details(req.params.id, function (child) {
+    res.send({
+      result: child,
+    });
+  });
+};
+exports.delete = function (req, res) {
+  Theloai.delete(req.params.id, function (deleteItem) {
+    res.send({
+      result: deleteItem,
+    });
+  });
+};
+exports.addTheLoai = function (req, res) {
+  var data = req.body;
+  Theloai.add_new(data, function (response) {
+    res.send({
+      result: response,
+    });
+  });
+};
--- BackEnd/app/router/theloai.router.js
+++ BackEnd/app/router/theloai.router.js
@@ -1,5 +1,7 @@
 module.exports = function (router) {
   var theloaiController = require("../controller/theloai.controller");
+  router.post("/theloai/add", theloaiController.addTheLoai);
+
   router.get("/theloai/all", theloaiController.get_all);
   router.get("/theloai/list", theloaiController.get_header);
   router.get("/theloai/details/:id", theloaiController.details);
